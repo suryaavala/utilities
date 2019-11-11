16a159af2bffade8926b588b58d916ac253c3fc3
Surya <surya.avala@eliiza.com.au>
2019-11-11 14:45:37 +1100

    Docker hot reload
    - build_and_run.sh automatically deleletes old docker container - builds
    and new one and runs the new
    - hot_reload watches a directory for changes and then triggers
    build_and_run.sh

---
+++ docker/build_and_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+imageName=propensity-minimal:latest
+containerName=$(docker ps --filter "ancestor=${imageName}" -q --format '{{.Names}}')
+
+docker build -t $imageName -f Dockerfile  .
+
+echo Delete old container...
+docker rm -f $containerName
+
+echo Run new container...
+docker run -d -p 8080:8080 --name $containerName $imageName serve
